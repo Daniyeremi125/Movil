c3489bcfa3c1e764ccf8c3382de015051de094eb
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-06-12 11:05:43 -0500

    video2

--- Dart_POO.dart/Tareaclase.dart
+++ Dart_POO.dart/Tareaclase.dart
@@ -1,24 +1,40 @@
-void main (){
+void main(){
+
+Telefono miTelefono = Telefono('Redmi', '22323232323', true);
+
+print(miTelefono);
+print(miTelefono.marca);
+print(miTelefono.numero);
+print(miTelefono.cargaRapida);
+
+miTelefono.marca = 'LG';
+print(miTelefono.marca);
+miTelefono.llamar();
+String numeroTelefono = miTelefono.obtenerNumero();
+print(numeroTelefono);
+
+Telefono  poco= Telefono('LG', '223333323', false);
+
+print(poco);
+print(poco.marca);
 
 }
-
 class Telefono{
   //Atributos
   String marca;
   String numero;
-  bool caragaRapida;
+  bool cargaRapida;
 
-  //
+  //Constructor
 
   Telefono(this.marca, this.numero, this.cargaRapida);
 
-  //Metodo
-
+  //metodos
   void llamar(){
-    print("El telefono esta llamando...");
+    print('El telefono esta llamando..');
   }
 
-  String obtenerNumero(){
-    return
+  obtenerNumero(){
+    return numero;
   }
 }

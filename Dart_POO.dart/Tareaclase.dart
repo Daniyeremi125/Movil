10dd1461104f019a3193e140020e19e29c1e27d2
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-06-12 10:12:42 -0500

    video1

---
+++ Dart_POO.dart/Tareaclase.dart
@@ -0,0 +1,24 @@
+void main (){
+
+}
+
+class Telefono{
+  //Atributos
+  String marca;
+  String numero;
+  bool caragaRapida;
+
+  //
+
+  Telefono(this.marca, this.numero, this.cargaRapida);
+
+  //Metodo
+
+  void llamar(){
+    print("El telefono esta llamando...");
+  }
+
+  String obtenerNumero(){
+    return
+  }
+}

a3820c83a4d7bf0112bb697161afba5d7fd7286b
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-06-13 00:17:46 -0500

    Datatime11

---
+++ Dart_POO.dart/011Datatime.dart
@@ -0,0 +1,26 @@
+void main(){
+
+  DateTime fechaActual = DateTime.now();
+
+  print(fechaActual.year);
+  
+  print(fechaActual.month);
+  print(fechaActual.day);
+  print(fechaActual.hour);
+  print(fechaActual.minute);
+
+  String fecha = "2020-07-20 20:12:23";
+  
+  DateTime fechaPasada = DateTime.parse(fecha);
+
+  print(fechaPasada);
+
+  DateTime dt1 = DateTime.parse("2020-07-20 20:12:23");
+  DateTime dt2 = DateTime.parse("2021-05-11 11:12:23");
+
+  Duration diferencia= dt2.difference(dt1);
+
+  print(diferencia.inDays);
+  print(diferencia.inHours);
+  print(diferencia.inMinutes);
+}

7f654a2b51ce1181304001d73dec50a598aa74c1
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-06-12 23:58:53 -0500

    Elearning7

---
+++ Dart_POO.dart/007Elearning.dart
@@ -0,0 +1,20 @@
+void main() {
+   Perro fido = Perro(peso:20, nombrex: "Vinosolo", tipox: "Mamifero");
+   print(fido.nombre);
+}
+//clase padre
+class Animal{
+
+String? nombre;
+String? tipo;
+
+Animal({this.nombre, this.tipo});
+
+}
+
+//clase hija
+
+class Perro extends Animal{
+  double? peso;
+  Perro({this.peso, nombrex, tipox}) : super(nombre: nombrex,tipo: tipox);
+}

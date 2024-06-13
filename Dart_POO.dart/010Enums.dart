6cd7916f0e06c64b4c76f539476d7f1427886424
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-06-13 00:13:25 -0500

    Enums10

---
+++ Dart_POO.dart/010Enums.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  print(Colores.values);
+  Colores.values.forEach((item){
+
+//    print(item.index);
+
+print(item.name);
+  });
+}
+
+enum Colores {
+  rojo,
+  azul,
+  amarillo
+}
